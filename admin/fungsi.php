8f24f5c8194695ead4b4be22fe04b28381990773
Joshua Davian <44218146+joedave13@users.noreply.github.com>
2020-05-21 18:39:54 +0700

    Menambah Hitung Bobot dan Panjang Vektor

--- admin/fungsi.php
+++ admin/fungsi.php
@@ -1,84 +1,146 @@
-<?php 
-    function preproses($teks)
-    {
-        include '../koneksi.php';
-        //Bersihkan tanda baca, ganti dengan space
-        $teks = str_replace("'", " ", $teks);
-        $teks = str_replace("-", " ", $teks);
-        $teks = str_replace(")", " ", $teks);
-        $teks = str_replace("(", " ", $teks);
-        $teks = str_replace("\"", " ", $teks);
-        $teks = str_replace("/", " ", $teks);
-        $teks = str_replace("=", " ", $teks);
-        $teks = str_replace(".", " ", $teks);
-        $teks = str_replace(",", " ", $teks);
-        $teks = str_replace(":", " ", $teks);
-        $teks = str_replace(";", " ", $teks);
-        $teks = str_replace("!", " ", $teks);
-        $teks = str_replace("?", " ", $teks);
+<?php
+function preproses($teks)
+{
+    include '../koneksi.php';
+    //Bersihkan tanda baca, ganti dengan space
+    $teks = str_replace("'", " ", $teks);
+    $teks = str_replace("-", " ", $teks);
+    $teks = str_replace(")", " ", $teks);
+    $teks = str_replace("(", " ", $teks);
+    $teks = str_replace("\"", " ", $teks);
+    $teks = str_replace("/", " ", $teks);
+    $teks = str_replace("=", " ", $teks);
+    $teks = str_replace(".", " ", $teks);
+    $teks = str_replace(",", " ", $teks);
+    $teks = str_replace(":", " ", $teks);
+    $teks = str_replace(";", " ", $teks);
+    $teks = str_replace("!", " ", $teks);
+    $teks = str_replace("?", " ", $teks);
 
-        //Ubah ke huruf kecil
-        $teks = strtolower(trim($teks));
-        
-        //Stopword remove
-        $stoplist = array ("yang", "juga", "dari", "dia", "kami", "kamu", "ini", "itu", "atau", "dan", "tersebut", "pada", "dengan", "adalah", "yaitu", "ke");
+    //Ubah ke huruf kecil
+    $teks = strtolower(trim($teks));
 
-        foreach ($stoplist as $i => $value) {
-            $teks = str_replace($stoplist[$i], "", $teks);
-        }
+    //Stopword remove
+    $stoplist = array("yang", "juga", "dari", "dia", "kami", "kamu", "ini", "itu", "atau", "dan", "tersebut", "pada", "dengan", "adalah", "yaitu", "ke");
 
-        //Terapkan stemming (ubah ke kata dasar)
-        $restem = mysqli_query($koneksi, "SELECT * FROM tb_stem ORDER BY id");
-
-        while($rowstem = mysqli_fetch_array($restem)){
-            $teks = str_replace($rowstem['term'], $rowstem['stem'], $teks);
-        }
-
-        //Return teks
-        $teks = strtolower(trim($teks));
-        return $teks;
+    foreach ($stoplist as $i => $value) {
+        $teks = str_replace($stoplist[$i], "", $teks);
     }
 
-    function buatIndex()
-    {
-        include '../koneksi.php';
+    //Terapkan stemming (ubah ke kata dasar)
+    $restem = mysqli_query($koneksi, "SELECT * FROM tb_stem ORDER BY id");
 
-        //Hapus index sebelumnya
-        mysqli_query($koneksi, "TRUNCATE TABLE tb_index");
+    while ($rowstem = mysqli_fetch_array($restem)) {
+        $teks = str_replace($rowstem['term'], $rowstem['stem'], $teks);
+    }
 
-        //Ambil semua dokumen (teks)
-        $resDokumen = mysqli_query($koneksi, "SELECT * FROM tb_dokumen ORDER BY id");
-        $num_rows = mysqli_num_rows($resDokumen);
-        print("Mengindeks sebanyak " . $num_rows . " dokumen. <br>");
+    //Return teks
+    $teks = strtolower(trim($teks));
+    return $teks;
+}
 
-        while ($row = mysqli_fetch_array($resDokumen)) {
-            $docId = $row['id'];
-            $dokumen = $row['dokumen'];
+function buatIndex()
+{
+    include '../koneksi.php';
 
-            //Preprocessing
-            $dokumen = preproses($dokumen);
+    //Hapus index sebelumnya
+    mysqli_query($koneksi, "TRUNCATE TABLE tb_index");
 
-            //Simpan ke tabel index
-            $aDokumen = explode(" ", trim($dokumen));
+    //Ambil semua dokumen (teks)
+    $resDokumen = mysqli_query($koneksi, "SELECT * FROM tb_dokumen ORDER BY id");
+    $num_rows = mysqli_num_rows($resDokumen);
+    print("Mengindeks sebanyak " . $num_rows . " dokumen. <br>");
 
-            foreach ($aDokumen as $j => $value) {
-                if ($aDokumen[$j] != "") {
-                    //Jumlah baris yang dikembalikan query tersebut
-                    $rescount = mysqli_query($koneksi, "SELECT jumlah FROM tb_index WHERE term = '$aDokumen[$j]' AND id_doc = $docId");
-                    $num_rows = mysqli_num_rows($rescount);
+    while ($row = mysqli_fetch_array($resDokumen)) {
+        $docId = $row['id'];
+        $dokumen = $row['dokumen'];
 
-                    if ($num_rows > 0) {
-                        $rowcount = mysqli_fetch_array($rescount);
-                        $count = $rowcount['jumlah'];
-                        $count++;
+        //Preprocessing
+        $dokumen = preproses($dokumen);
 
-                        mysqli_query($koneksi, "UPDATE tb_index SET jumlah = $count WHERE term = '$aDokumen[$j]' AND id_doc = $docId");
-                    }
-                    else {
-                        mysqli_query($koneksi, "INSERT INTO tb_index (term, id_doc, jumlah) VALUES ('$aDokumen[$j]', $docId, 1)");
-                    }
+        //Simpan ke tabel index
+        $aDokumen = explode(" ", trim($dokumen));
+
+        foreach ($aDokumen as $j => $value) {
+            if ($aDokumen[$j] != "") {
+                //Jumlah baris yang dikembalikan query tersebut
+                $rescount = mysqli_query($koneksi, "SELECT jumlah FROM tb_index WHERE term = '$aDokumen[$j]' AND id_doc = $docId");
+                $num_rows = mysqli_num_rows($rescount);
+
+                if ($num_rows > 0) {
+                    $rowcount = mysqli_fetch_array($rescount);
+                    $count = $rowcount['jumlah'];
+                    $count++;
+
+                    mysqli_query($koneksi, "UPDATE tb_index SET jumlah = $count WHERE term = '$aDokumen[$j]' AND id_doc = $docId");
+                } else {
+                    mysqli_query($koneksi, "INSERT INTO tb_index (term, id_doc, jumlah) VALUES ('$aDokumen[$j]', $docId, 1)");
                 }
             }
         }
     }
-?>
+}
+
+function hitungBobot()
+{
+    include '../koneksi.php';
+    //Hitung total doc id
+    $resn = mysqli_query($koneksi, "SELECT DISTINCT id_doc FROM tb_index");
+    $n = mysqli_num_rows($resn);
+
+    //Ambil setiap record dalam tb_index
+    //Hitung bobot untuk setiap term
+    $resBobot = mysqli_query($koneksi, "SELECT * FROM tb_index ORDER BY id");
+    $num_rows = mysqli_num_rows($resBobot);
+    print("Terdapat " . $num_rows . " term yang diberikan bobot. <br>");
+
+    while ($rowBobot = mysqli_fetch_array($resBobot)) {
+        //$w = tf * log(n/N)
+        $term = $rowBobot['term'];
+        $tf = $rowBobot['jumlah'];
+        $id = $rowBobot['id'];
+        
+        //Jumlah dokumen yang mengandung term tersebut (N)
+        $resNTerm = mysqli_query($koneksi, "SELECT COUNT(*) AS N FROM tb_index WHERE term = '$term'");
+        $rowNTerm = mysqli_fetch_array($resNTerm);
+        $NTerm = $rowNTerm['N'];
+        
+        $w = $tf * log($n/$NTerm);
+
+        //Update bobot
+        $resUpdateBobot = mysqli_query($koneksi, "UPDATE tb_index SET bobot = $w WHERE id = $id");
+    }
+}
+
+function panjangVektor()
+{
+    include '../koneksi.php';
+    //Hapus isi tabel vektor
+    mysqli_query($koneksi, "TRUNCATE TABLE tb_vektor");
+
+    //Ambil setiap doc id dari table index
+    //Hitung panjang vektor untuk setiap doc id
+    //Simpan ke table vektor
+    $resDocId = mysqli_query($koneksi, "SELECT DISTINCT id_doc FROM tb_index");
+
+    $num_rows = mysqli_num_rows($resDocId);
+    print('Terdapat ' . $num_rows . " dokumen yang dihitung panjang vektornya. <br>");
+
+    while ($rowDocId = mysqli_fetch_array($resDocId)) {
+        $docId = $rowDocId['id_doc'];
+
+        $resVektor = mysqli_query($koneksi, "SELECT bobot FROM tb_index WHERE id_doc = $docId");
+
+        //Jumlahkan semua bobot kuadrat
+        $panjangVektor = 0;
+        while ($rowVektor = mysqli_fetch_array($resVektor)) {
+            $panjangVektor = $panjangVektor + $rowVektor['bobot'] * $rowVektor['bobot'];
+        }
+
+        //Hitung akarnya
+        $panjangVektor = sqrt($panjangVektor);
+
+        //Masukkan ke dalam tabel vektor
+        $resInsertVektor = mysqli_query($koneksi, "INSERT INTO tb_vektor (doc_id, panjang) VALUES ($docId, $panjangVektor)");
+    }
+}
--- admin/master_hitung.php
+++ admin/master_hitung.php
@@ -16,6 +16,104 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-title text-center">Bobot Dokumen</div>
+                </div>
+                <div class="card-body">
+                    <?php hitungBobot(); ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-title text-center">Jumlah Vektor</div>
+                </div>
+                <div class="card-body">
+                    <?php panjangVektor(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-3">
+        <div class="row">
+            <a href="master_index.php" class="btn btn-danger mb-3">
+                Kembali
+            </a>
+            <button type="button" class="btn btn-info mb-3 mx-2" data-toggle="modal" data-target="#modalVektor">
+                Hasil Count Vektor
+            </button>
+
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Term</th>
+                        <th>Doc-id</th>
+                        <th>Count</th>
+                        <th>Bobot</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        include '../koneksi.php';
+                        $i = 1;
+                        $result = mysqli_query($koneksi, "SELECT * FROM tb_index ORDER BY id");
+                        while($row = mysqli_fetch_array($result)){ 
+                    ?>
+                    <tr>
+                        <td><?= $i++; ?></td>
+                        <td><?= $row['term']; ?></td>
+                        <td><?= $row['id_doc']; ?></td>
+                        <td><?= $row['jumlah']; ?></td>
+                        <td><?= $row['bobot']; ?></td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalVektor" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Panjang Vektor TF-IDF</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID Dokumen</th>
+                                <th>Panjang Vektor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                                include '../koneksi.php';
+                                $result = mysqli_query($koneksi, "SELECT * FROM tb_vektor");
+                                while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <tr>
+                                <td><?= $row['doc_id']; ?></td>
+                                <td><?= $row['panjang']; ?></td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
     </div>
 
 </div>
