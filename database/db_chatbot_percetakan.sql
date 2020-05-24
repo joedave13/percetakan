063b77a82963b99b42df0894221c9552050a96e1
Joshua Davian <44218146+joedave13@users.noreply.github.com>
2020-05-24 19:00:27 +0700

    Update Database

--- database/db_chatbot_percetakan.sql
+++ database/db_chatbot_percetakan.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Bulan Mei 2020 pada 13.41
--- Versi server: 10.4.11-MariaDB
--- Versi PHP: 7.4.1
+-- Generation Time: May 24, 2020 at 01:59 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_admin`
+-- Table structure for table `tb_admin`
 --
 
 CREATE TABLE `tb_admin` (
@@ -36,7 +36,7 @@ CREATE TABLE `tb_admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_admin`
+-- Dumping data for table `tb_admin`
 --
 
 INSERT INTO `tb_admin` (`admin_id`, `admin_nama`, `admin_username`, `admin_password`) VALUES
@@ -45,7 +45,37 @@ INSERT INTO `tb_admin` (`admin_id`, `admin_nama`, `admin_username`, `admin_passw
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_dokumen`
+-- Table structure for table `tb_cache`
+--
+
+CREATE TABLE `tb_cache` (
+  `id` int(11) NOT NULL,
+  `query` varchar(255) NOT NULL,
+  `doc_id` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_cache`
+--
+
+INSERT INTO `tb_cache` (`id`, `query`, `doc_id`, `nilai`) VALUES
+(1, 'selamat pagi', 1, 0.999999),
+(2, 'selamat sore', 1, 0.707106),
+(4, 'jam berapa toko buka', 2, 0.285453),
+(5, 'jam toko buka', 2, 0.285453),
+(6, 'harga banner korea', 12, 0.851645),
+(7, 'harga art paper', 12, 0.491698),
+(8, 'halo', 13, 0.513458),
+(13, 'harga cetak banner glossy', 2, 0.00745103),
+(14, 'harga cetak banner glossy', 12, 0.316488),
+(15, 'harga cetak banner glossy', 13, 0.00745103),
+(16, 'harga cetak banner glossy', 14, 0.79064);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_dokumen`
 --
 
 CREATE TABLE `tb_dokumen` (
@@ -54,18 +84,20 @@ CREATE TABLE `tb_dokumen` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_dokumen`
+-- Dumping data for table `tb_dokumen`
 --
 
 INSERT INTO `tb_dokumen` (`id`, `dokumen`) VALUES
 (1, 'selamat pagi'),
-(2, 'percetakan radja printing buka jam 07.00'),
-(12, 'mencetak banner korea harganya 30000');
+(2, 'percetakan radja printing buka jam 7 pagi'),
+(12, 'mencetak banner korea harganya 30000'),
+(13, 'halo, selamat datang di percetakan radja printing'),
+(14, 'mencetak banner glossy harga 25000');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_index`
+-- Table structure for table `tb_index`
 --
 
 CREATE TABLE `tb_index` (
@@ -77,29 +109,41 @@ CREATE TABLE `tb_index` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_index`
+-- Dumping data for table `tb_index`
 --
 
 INSERT INTO `tb_index` (`id`, `term`, `id_doc`, `jumlah`, `bobot`) VALUES
-(1, 'selamat', 1, 1, 1.09861),
-(2, 'pagi', 1, 1, 1.09861),
-(3, 'cetak', 2, 1, 0.405465),
-(4, 'radja', 2, 1, 1.09861),
-(5, 'printing', 2, 1, 1.09861),
-(6, 'buka', 2, 1, 1.09861),
-(7, 'jam', 2, 1, 1.09861),
-(8, '07', 2, 1, 1.09861),
-(9, '00', 2, 1, 1.09861),
-(10, 'cetak', 12, 1, 0.405465),
-(11, 'banner', 12, 1, 1.09861),
-(12, 'korea', 12, 1, 1.09861),
-(13, 'harga', 12, 1, 1.09861),
-(14, '30000', 12, 1, 1.09861);
+(1, 'selamat', 1, 1, 0.916291),
+(2, 'pagi', 1, 1, 0.916291),
+(3, 'cetak', 2, 1, 0.223144),
+(4, 'radja', 2, 1, 0.916291),
+(5, 'printing', 2, 1, 0.916291),
+(6, 'buka', 2, 1, 1.60944),
+(7, 'jam', 2, 1, 1.60944),
+(8, '7', 2, 1, 1.60944),
+(9, 'pagi', 2, 1, 0.916291),
+(10, 'cetak', 12, 1, 0.223144),
+(11, 'banner', 12, 1, 0.916291),
+(12, 'korea', 12, 1, 1.60944),
+(13, 'harga', 12, 1, 0.916291),
+(14, '30000', 12, 1, 1.60944),
+(15, 'halo', 13, 1, 1.60944),
+(16, 'selamat', 13, 1, 0.916291),
+(17, 'datang', 13, 1, 1.60944),
+(18, 'di', 13, 1, 1.60944),
+(19, 'cetak', 13, 1, 0.223144),
+(20, 'radja', 13, 1, 0.916291),
+(21, 'printing', 13, 1, 0.916291),
+(22, 'cetak', 14, 1, 0.223144),
+(23, 'banner', 14, 1, 0.916291),
+(24, 'glossy', 14, 1, 1.60944),
+(25, 'harga', 14, 1, 0.916291),
+(26, '25000', 14, 1, 1.60944);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_stem`
+-- Table structure for table `tb_stem`
 --
 
 CREATE TABLE `tb_stem` (
@@ -109,7 +153,7 @@ CREATE TABLE `tb_stem` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_stem`
+-- Dumping data for table `tb_stem`
 --
 
 INSERT INTO `tb_stem` (`id`, `term`, `stem`) VALUES
@@ -121,7 +165,7 @@ INSERT INTO `tb_stem` (`id`, `term`, `stem`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_vektor`
+-- Table structure for table `tb_vektor`
 --
 
 CREATE TABLE `tb_vektor` (
@@ -130,81 +174,95 @@ CREATE TABLE `tb_vektor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_vektor`
+-- Dumping data for table `tb_vektor`
 --
 
 INSERT INTO `tb_vektor` (`doc_id`, `panjang`) VALUES
-(1, 1.55367),
-(2, 2.72141),
-(12, 2.23432);
+(1, 1.29583),
+(2, 3.2155),
+(12, 2.62861),
+(13, 3.2155),
+(14, 2.62861);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `tb_admin`
+-- Indexes for table `tb_admin`
 --
 ALTER TABLE `tb_admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
--- Indeks untuk tabel `tb_dokumen`
+-- Indexes for table `tb_cache`
+--
+ALTER TABLE `tb_cache`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_dokumen`
 --
 ALTER TABLE `tb_dokumen`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tb_index`
+-- Indexes for table `tb_index`
 --
 ALTER TABLE `tb_index`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tb_stem`
+-- Indexes for table `tb_stem`
 --
 ALTER TABLE `tb_stem`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tb_vektor`
+-- Indexes for table `tb_vektor`
 --
 ALTER TABLE `tb_vektor`
   ADD PRIMARY KEY (`doc_id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `tb_admin`
+-- AUTO_INCREMENT for table `tb_admin`
 --
 ALTER TABLE `tb_admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_dokumen`
+-- AUTO_INCREMENT for table `tb_cache`
 --
-ALTER TABLE `tb_dokumen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+ALTER TABLE `tb_cache`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_index`
+-- AUTO_INCREMENT for table `tb_dokumen`
 --
-ALTER TABLE `tb_index`
+ALTER TABLE `tb_dokumen`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_stem`
+-- AUTO_INCREMENT for table `tb_index`
+--
+ALTER TABLE `tb_index`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `tb_stem`
 --
 ALTER TABLE `tb_stem`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_vektor`
+-- AUTO_INCREMENT for table `tb_vektor`
 --
 ALTER TABLE `tb_vektor`
-  MODIFY `doc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `doc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
